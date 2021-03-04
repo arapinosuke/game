f27bfa18d3d10b0de545e1e08edbae535bf5921e
arapinosuke <67851390+arapinosuke@users.noreply.github.com>
2021-03-05 00:32:38 +0900

    a

---
+++ pazzlegame/Assets/Scripts/BallController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BallController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public bool IsTouch = false;
+
+    RectTransform RectTransform;
+
+    // 移動用のVectro2
+    public Vector2 CurrentPos;
+
+    // 盤面判定用のVector2(0,0)は一番左上の左端
+    public Vector2 BoardPos;
+
+    public bool DestroyFlag = false;
+
+    public Sprite[] sprites = new Sprite[6];
+
+    public enum BallType
+    {
+        Invalide = -1,
+        Water,
+        Wind,
+        Fire,
+        Dark,
+        Light,
+        Heal,
+        Num
+    }
+
+    public BallType ThisBallType = BallType.Invalide;
+
+
+    private void Awake()
+    {
+        RectTransform = this.GetComponent<RectTransform>();
+
+        SetRandomType();
+
+        StartCoroutine(SetCurrentPos());
+    }
+
+    public void SetRandomType()
+    {
+        var randomType = Random.Range(0, (int)BallType.Num);
+
+        GetComponent<Image>().sprite = sprites[randomType];
+
+        ThisBallType = (BallType)randomType;
+    }
+
+
+    IEnumerator SetCurrentPos()
+    {
+        yield return new WaitForEndOfFrame();
+        CurrentPos = RectTransform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (IsTouch)
+        {
+            RectTransform.position = Input.mousePosition;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        IsTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        IsTouch = false;
+        this.transform.position = CurrentPos;
+    }
+
+    public void SetPos(Vector3 nextPos)
+    {
+        this.RectTransform.position = nextPos;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsTouch)
+        {
+            var afterPos = CurrentPos;
+            collision.GetComponent<BallController>().SetPos(CurrentPos);
+            CurrentPos = collision.GetComponent<BallController>().CurrentPos;
+            collision.GetComponent<BallController>().CurrentPos = afterPos;
+
+        }
+    }
+}
---
+++ pazzlegame/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject Ball;
+    public GameObject CharacterParent;
+
+    private void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(Ball, CharacterParent.transform);
+        }
+    }
+}
